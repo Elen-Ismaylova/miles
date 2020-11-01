8317f39cc610c60bed917dd77d392318625b2d59
Elen Ismaylova <wonderelen@gmail.com>
2020-11-01 14:31:51 +0300

    files added

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean registered = true;
+        int price = 10000;
+        int bonus = 20;
+        int miles = 1;
+        int bonusMile = price * miles / bonus;
+                System.out.println(bonusMile);
+
+    }
+}
